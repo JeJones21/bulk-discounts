a07286723e443411fc9669c5e6d977e22070e561
Jessica Jones <daddehjones@gmail.com>
2021-09-26 15:55:48 -0400

    Add bulk discount route.

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -5,5 +5,4 @@ RSpec.describe BulkDiscount, type: :model do
   it { should have_many(:items).through(:merchant) }
   it { should have_many(:invoice_items).through(:items) }
   it { should have_many(:invoices).through(:invoice_items) }
-
 end
