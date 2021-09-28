a5bf3a3952d41029ad645be5c974bf065542b6c2
Jessica Jones <daddehjones@gmail.com>
2021-09-27 21:46:51 -0400

    Add validations.

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,4 +3,7 @@ class BulkDiscount < ApplicationRecord
   has_many :items, through: :merchant
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
+
+  validates_presence_of :percentage_discount
+  validates_presence_of :quantity_threshold
 end
