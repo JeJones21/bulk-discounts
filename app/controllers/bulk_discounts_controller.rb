d978b4692aedc9a84b89d2199b8ace4993307379
Jessica Jones <daddehjones@gmail.com>
2021-09-27 23:42:19 -0400

    Add merchant to show controller.

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,11 +1,12 @@
 class BulkDiscountsController < ApplicationController
   before_action :current_merchant
-  
+
   def index
     @bulk_discounts = @merchant.bulk_discounts.all
   end
 
   def show
+    @merchant = Merchant.find(params[:merchant_id])
     @bulk_discount = BulkDiscount.find(params[:id])
   end
 
