fe31fd8b1c146c30fb7fdccb79f3eef714354783
Jessica Jones <daddehjones@gmail.com>
2021-09-27 21:47:30 -0400

    Create BulkDiscountsController.

---
+++ app/controllers/bulk_discounts_controller.rb
@@ -0,0 +1,11 @@
+class BulkDiscountsController < ApplicationController
+
+  def index
+    @bulk_discounts = BulkDiscount.all
+  end
+
+  def show
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+  
+end
