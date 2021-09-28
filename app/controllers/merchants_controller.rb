36dbf865dfe3ca438b88d67ce90605d7e9f966c8
Jessica Jones <daddehjones@gmail.com>
2021-09-28 16:19:21 -0400

    Create merchant controller.

---
+++ app/controllers/merchants_controller.rb
@@ -0,0 +1,12 @@
+class MerchantsController < ApplicationController
+  before_action :current_merchant, only: :show
+
+  def index
+    @merchants = Merchant.all
+  end
+
+  def show
+    @customers = @merchant.favorite_customers
+    @items = @merchant.items_ready_to_ship
+  end
+end
