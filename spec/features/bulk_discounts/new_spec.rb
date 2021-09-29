ae449d44054272f298a20b1a512e7899053f867f
Jessica Jones <daddehjones@gmail.com>
2021-09-28 21:14:10 -0400

    Update new form expectations.

--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -1,26 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'BulkDiscount#new' do
-  @m1 = Merchant.create!(name: 'Creepy Cuddles')
+  before :each do
+    @m1 = Merchant.create!(name: 'Creepy Cuddles')
 
-end
+    visit new_merchant_bulk_discount_path(@m1)
+  end
 
-it "has a form to create a new bulk discount" do
-  visit merchant_bulk_discounts_path(@m1)
-
-    click_link "Create Bulk Discount"
-
-    expect(current_path).to eq(new_merchant_bulk_discount_path(@m1))
+  it "has a form to create a new bulk discount" do
 
     fill_in 'Name', with: 'Drip More For Less'
-    fill_in 'Percentage Discount', with: '50'
-    fill_in 'Quantity Threshold', with: '20'
+    fill_in 'Percentage discount', with: 50
+    fill_in 'Quantity threshold', with: 20
 
     click_button 'Submit'
 
     expect(current_path).to eq(merchant_bulk_discounts_path(@m1))
-    expect(page).to have_content("Discount Percentage: 20")
-    expect(page).to have_content("Drip More For Less")
-    expect(page).to have_content(BulkDiscount.last.quantity_threshold)
 
+    expect(page).to have_content(50)
+    expect(page).to have_content(20)
+    save_and_open_page
+    
+    expect(page).to have_content("Drip More For Less")
+    # expect(page).to have_content(BulkDiscount.last.quantity_threshold)
+  end
 end
