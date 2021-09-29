5f9aab1b2e0e351fcd416535da4ef529a8a446fd
Jessica Jones <daddehjones@gmail.com>
2021-09-29 15:37:28 -0400

    Create discount revenue plus 2 helper methods.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -14,4 +14,24 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def total_rev
+   unit_price * quantity
+ end
+
+ def discount_finder
+   item.merchant.bulk_discounts
+   .where('bulk_discounts.quantity_threshold <= ?', quantity)
+   .select('bulk_discounts.*')
+   .order(percentage_discount: :desc)
+   .first
+ end
+
+ def disco_rev
+    if discount_finder.blank?
+      total_rev
+    else
+      (total_rev - (total_rev * (discount_finder.percentage_discount.to_f/100)))
+    end
+  end
 end
