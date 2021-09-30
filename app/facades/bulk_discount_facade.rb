48f98dc9f26d3415da4c0fca9e70611ac1230208
Jessica Jones <daddehjones@gmail.com>
2021-09-30 00:23:17 -0400

    Create facade.

--- app/facades/bulk_discount_facade.rb
+++ app/facades/bulk_discount_facade.rb
@@ -1,3 +1,11 @@
 class BulkDiscountFacade
 
+  def self.holidays
+
+    json = BulkDiscountService.next_three_holidays
+
+    @holidays = json.map do |data|
+      Holiday.new(data)
+   end
+  end
 end
